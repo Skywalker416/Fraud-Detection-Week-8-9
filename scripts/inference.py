1eb74cf28677d5a44d038a74c9d6416662bc67c0
Amanuel Legesse <85198830+Skywalker416@users.noreply.github.com>
2025-03-11 20:36:18 +0300

    Update inference.py
    For making predictions on new data.

--- scripts/inference.py
+++ scripts/inference.py
@@ -1,5 +1,7 @@
+import joblib
 import pandas as pd
 
-def predict_fraud(model, new_data: pd.DataFrame):
-    """Use trained model to predict fraud."""
+def predict_new_data(new_data: pd.DataFrame):
+    """Load model and make predictions."""
+    model = joblib.load('../models/fraud_model.pkl')
     return model.predict(new_data)
